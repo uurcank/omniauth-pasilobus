d99506cb4c5dbe510259db06807c725a2eb45042
Ugurcan Kaya <uurcan@pasilobus.com>
2021-02-19 06:25:16 -0800

    revert callback_url change

--- lib/omniauth/pasilobus.rb
+++ lib/omniauth/pasilobus.rb
@@ -32,6 +32,10 @@ module OmniAuth
         true
       end
       
+      def callback_url
+        options[:redirect_uri] || (full_host + script_name + callback_path)
+      end
+      
     end
   end
 end
